d58415e14090ed0185aec6ef5330031434210d64
janisZem <jz10024@lu.lv>
2016-04-25 16:06:01 +0300

    attribute merge into one dimesion

--- app/Http/Controllers/AttrMerge.php
+++ app/Http/Controllers/AttrMerge.php
@@ -10,15 +10,64 @@ use DB;
 class AttrMerge extends Controller {
 
     public function merge(&$rels, &$mClasses) {
-        $p = 'name';
-
-        foreach ($mClasses as $c) {
+        foreach ($mClasses as $key => $c) {
             if ($c['key'][0] == 'a') {
                 $hier_array = $this->find_hierarchies(substr($c['text'], 2));
-                $this->create_hierarchies($hier_array, $rels, $mClasses);
+                if (count($hier_array) > 0) {
+                    $this->create_hierarchies($hier_array, $rels, $mClasses);
+                } else {
+                    $parent = $this->find_parents(substr($c['text'], 2));
+                    $mClasses[$key]['parent'] = $parent;
+                }
+            }
+        }
+        $this->merge_attrrs($rels, $mClasses);
+    }
+
+    private function merge_attrrs(&$rels, &$mClasses) {
+        $parents_array = [];
+        foreach ($mClasses as $c) {
+            if ($c['key'][0] == 'a' && isset($c['parent'])) {
+                if (!array_key_exists($c['parent'], $parents_array)) {
+                    $parents_array[$c['parent']][] = substr($c['text'], 2);
+                } else {
+                    array_push($parents_array[$c['parent']], substr($c['text'], 2));
+                }
             }
         }
 
+        foreach ($parents_array as $p) {
+            if (count($p) > 1) {
+                foreach ($mClasses as $key => $c) {
+                    if ($c['text'] == 'a_' . $p[0]) {
+                        for ($i = 1; $i < count($p); $i++) {
+                            $mClasses[$key]['text'] .= ' \\n a_' . $p[$i];
+                            $this->delete_class($rels, $mClasses, $p[$i]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private function delete_class(&$rels, &$mClasses, $class) {
+        foreach ($mClasses as $key => $c) {
+            if ($mClasses[$key]['text'] == 'a_' . $class) {
+                unset($mClasses[$key]);
+            }
+        }
+        foreach($rels as $key => $r){
+            if($r['to'] == 'a_' .$class){
+                unset($rels[$key]);
+            }
+        }
+    }
+
+    private function find_parents($attr) {
+        $res2 = DB::connection('neo4j')->select('MATCH (n:attr { title: \'' . $attr . '\' })-[:hasAttr]-(neighbors) RETURN neighbors');
+        if (isset($res2[0])) {
+            return $res2[0][0]->title;
+        }
     }
 
     private function create_hierarchies($hier_array, &$rels, &$mClasses) {
